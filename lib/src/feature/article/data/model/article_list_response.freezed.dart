3c23a4a10faba40920944acdb6abc874ff7a3a29
Nicodemus <nicodemusdemuse@gmail.com>
2025-11-12 08:11:52 +0700

    rebuild freezed

--- lib/src/feature/article/data/model/article_list_response.freezed.dart
+++ lib/src/feature/article/data/model/article_list_response.freezed.dart
@@ -118,10 +118,7 @@ return $default(_that);case _:
 final _that = this;
 switch (_that) {
 case _ArticleListResponse():
-return $default(_that);case _:
-  throw StateError('Unexpected subclass');
-
-}
+return $default(_that);}
 }
 /// A variant of `map` that fallback to returning `null`.
 ///
@@ -180,10 +177,7 @@ return $default(_that.articles,_that.page,_that.limit,_that.hasMore);case _:
 @optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( List<Article>? articles,  int? page,  int? limit,  bool? hasMore)  $default,) {final _that = this;
 switch (_that) {
 case _ArticleListResponse():
-return $default(_that.articles,_that.page,_that.limit,_that.hasMore);case _:
-  throw StateError('Unexpected subclass');
-
-}
+return $default(_that.articles,_that.page,_that.limit,_that.hasMore);}
 }
 /// A variant of `when` that fallback to returning `null`
 ///
