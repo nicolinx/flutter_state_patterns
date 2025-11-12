a9d04c69b8d06b5275d4ca1dfd5411cb8cfeef24
Nicodemus <nicodemusdemuse@gmail.com>
2025-11-12 08:25:36 +0700

    add injector for article data layer

--- lib/src/core/di/injector.dart
+++ lib/src/core/di/injector.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_state_boilerplate/src/core/network/api_client.dart';
+import 'package:flutter_state_boilerplate/src/feature/article/data/repository/article_repository.dart';
+import 'package:flutter_state_boilerplate/src/feature/article/data/service/article_service.dart';
 import 'package:get_it/get_it.dart';
 
 final di = GetIt.instance;
@@ -6,4 +8,8 @@ final di = GetIt.instance;
 Future<void> initDI() async {
   // --- Core / Network ---
   di.registerLazySingleton(() => ApiClient());
+
+  // --- Feature: Article ---
+  di.registerLazySingleton(() => ArticleService(di<ApiClient>()));
+  di.registerLazySingleton(() => ArticleRepository(di<ArticleService>()));
 }
