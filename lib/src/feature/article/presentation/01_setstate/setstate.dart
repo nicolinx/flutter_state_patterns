43d5a07ba8a7ab3c1463233da38f2eca4a0bcb9d
Nicodemus <nicodemusdemuse@gmail.com>
2025-11-13 07:59:11 +0700

    modify

---
+++ lib/src/feature/article/presentation/01_setstate/setstate.dart
@@ -0,0 +1,6 @@
+export 'package:flutter_state_boilerplate/src/feature/article/presentation/01_setstate/controller/article_list_controller.dart';
+export 'package:flutter_state_boilerplate/src/feature/article/presentation/01_setstate/controller/article_detail_controller.dart';
+export 'package:flutter_state_boilerplate/src/feature/article/presentation/01_setstate/state/article_list_state.dart';
+export 'package:flutter_state_boilerplate/src/feature/article/presentation/01_setstate/state/article_detail_state.dart';
+export 'package:flutter_state_boilerplate/src/feature/article/presentation/01_setstate/page/article_list_page.dart';
+export 'package:flutter_state_boilerplate/src/feature/article/presentation/01_setstate/page/article_detail_page.dart';
